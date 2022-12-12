63a8cc054c34a95275be5ba137a657283897326d
SongJianHIT <2582231611@qq.com>
2022-12-12 17:11:05 +0800

    Redis环境配置

---
+++ src/main/java/tech/songjian/reggie/config/RedisConfig.java
@@ -0,0 +1,32 @@
+/**
+ * @projectName reggie_take_out
+ * @package tech.songjian.reggie.config
+ * @className tech.songjian.reggie.config.RedisConfig
+ */
+package tech.songjian.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * RedisConfig
+ * @description Redis配置累
+ * @author SongJian
+ * @date 2022/12/12 17:06
+ * @version
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
